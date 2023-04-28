be903a1d27141e1d40c7f3c4961ba75e14997734
Raj Ratan <raj808341@gmail.com>
2023-04-28 10:19:45 +0530

    map through button list

--- video-box/src/components/ButtonList.js
+++ video-box/src/components/ButtonList.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Button from "./Button";
+
+const list = [
+  "All",
+  "Live",
+  "News",
+  "Cricket",
+  "Soccer",
+  "Gaming",
+  "Website",
+  "Mixes",
+  "T-Series",
+  "Music",
+  "Javascript",
+];
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className=" mt-5 ml-5 flex gap-3 items-center justify-center">
+      {list.map((item, index) => (
+        <Button name={item} key={index} />
+      ))}
+    </div>
+  );
 };
 
 export default ButtonList;
