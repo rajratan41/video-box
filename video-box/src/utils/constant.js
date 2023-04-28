0c12887a4d855cbcb3701ebcb0ba8ad46c1694c1
Raj Ratan <raj808341@gmail.com>
2023-04-28 10:42:11 +0530

    added constant file

---
+++ video-box/src/utils/constant.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyCk9MILxqeFXDrNUN6fc3NOgRHexrLlr80";
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  GOOGLE_API_KEY;
