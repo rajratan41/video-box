0c59a50d7fdc432064c59baf3c35882aa2bc3c8d
Raj Ratan <raj808341@gmail.com>
2023-04-28 09:01:22 +0530

    imported dispatch and created toogleHandler

--- video-box/src/components/Head.js
+++ video-box/src/components/Head.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { FaBars, FaSearch, FaUser } from "react-icons/fa";
 
+import { useDispatch } from "react-redux";
+import { toogleMenu } from "../utils/appSlice";
+
 const Head = () => {
+  const dispatch = useDispatch();
+
+  const toogleMenuHandler = () => {
+    dispatch(toogleMenu());
+  };
+
   return (
     <div className="bg-white h-20 grid grid-flow-col items-center p-2 px-8 shadow-xl">
       <div className="flex items-center">
-        <h5 className="text-2xl">
+        <h5
+          onClick={() => toogleMenuHandler()}
+          className="text-2xl cursor-pointer"
+        >
           <FaBars />
         </h5>
-        <h2 className="text-2xl font-sans font-bold ml-6">Video Box</h2>
+
+        <a href="/">
+          <h2 className="text-2xl font-sans font-bold ml-6 cursor-pointer">
+            Video Box
+          </h2>
+        </a>
       </div>
       <div className="flex items-center">
         <input
