77774b5fc9d457f9821ad08fadb6c3d2e3b5d820
Raj Ratan <raj808341@gmail.com>
2023-04-28 16:45:47 +0530

    updated api key

--- video-box/src/utils/constant.js
+++ video-box/src/utils/constant.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyCk9MILxqeFXDrNUN6fc3NOgRHexrLlr80";
+const GOOGLE_API_KEY = "AIzaSyBR_UrkQQ8_Nh5On5E7bjQUgDFzcpP28L0";
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
